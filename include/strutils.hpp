855ac36a8e9ef567627643a2cdbf06d037b93806
bdatto <bob.dattore@gmail.com>
2016-08-11 17:18:59 -0600

    moved bit manipulations into bits.hpp, strget into strutils.hpp

--- include/strutils.hpp
+++ include/strutils.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <deque>
+#include <complex>
 
 namespace strutils {
 
@@ -42,6 +43,54 @@ extern std::string to_lower(const std::string& s);
 extern std::string to_upper(const std::string& s,size_t start,size_t num_chars);
 extern std::string to_upper(const std::string& s);
 extern std::string token(const std::string& s,const std::string& separator,size_t token_number);
+
+template <class Type>
+void strget(const char *str,Type& numeric_val,int num_chars)
+{
+  std::string cval(str,num_chars);
+  int n,decimal;
+  char *chr=const_cast<char *>(cval.c_str());
+  double mult;
+  bool neg_val=false,neg_exp=false;
+  bool reading_exponent=false;
+  int exp=0;
+
+  trim(cval);
+  num_chars=decimal=cval.length();
+  numeric_val=0;
+  for (n=0; n < num_chars; n++) {
+    if (chr[n] == '.') {
+	numeric_val*=0.1;
+	decimal=n;
+    }
+    else if (chr[n] == 'E')
+	reading_exponent=true;
+    else {
+	if (chr[n] == ' ' || chr[n] == '+' || chr[n] == '-') {
+	  if (chr[n] == '-') {
+	    if (!reading_exponent)
+		neg_val=true;
+	    else
+		neg_exp=true;
+	  }
+	  chr[n]='0';
+	}
+	if (!reading_exponent)
+	  numeric_val+=static_cast<Type>((chr[n]-48)*std::pow(10.,num_chars-n-1));
+	else
+	  exp+=static_cast<int>((chr[n]-48)*std::pow(10.,num_chars-n-1));
+    }
+  }
+  if (neg_val) numeric_val=-numeric_val;
+  if (decimal != num_chars) {
+    mult=std::pow(0.1,num_chars-decimal-1);
+    numeric_val*=static_cast<Type>(mult);
+  }
+  if (reading_exponent) {
+    if (neg_exp) exp=-exp;
+    numeric_val*=static_cast<Type>(std::pow(10.,exp));
+  }
+}
 
 } // end namespace strutils
 
